936422aad8d1bf7979dbe78f2398684387bac972
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-07-20 04:43:50 -0500

    just before wrecking stuff

--- src/BQ.Presentation/States/CombatState.vb
+++ src/BQ.Presentation/States/CombatState.vb
@@ -1,5 +1,6 @@
 ﻿Friend Class CombatState
     Inherits BaseGameState(Of IWorldModel)
+    Private CharacterIndex As Integer
 
     Public Sub New(parent As IGameController, setState As Action(Of String, Boolean), context As IUIContext(Of IWorldModel))
         MyBase.New(parent, setState, context)
@@ -17,10 +18,13 @@
         Dim font = Context.Font(BagelQuestFont)
         Dim enemies = Model.Enemy.Enemies
         Dim x = Context.ViewSize.Item1 \ 2 - font.TextWidth(ChrW(0)) * enemies.Count \ 2
+        Dim y = Context.ViewSize.Item2 \ 2 - font.Height \ 2
+        Dim glyphWidth = font.TextWidth(ChrW(0))
+        font.WriteText(displayBuffer, (x + glyphWidth * CharacterIndex, y + font.Height), ChrW(&H1C), White)
         For Each enemy In enemies
-            font.WriteText(displayBuffer, (x, 0), enemy.MaskGlyph, enemy.MaskHue)
-            font.WriteText(displayBuffer, (x, 0), enemy.Glyph, enemy.Hue)
-            x += font.TextWidth(ChrW(0))
+            font.WriteText(displayBuffer, (x, y), enemy.MaskGlyph, enemy.MaskHue)
+            font.WriteText(displayBuffer, (x, y), enemy.Glyph, enemy.Hue)
+            x += glyphWidth
         Next
     End Sub
 End Class

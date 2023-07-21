3aad0222025949f095d958efecf5d24297b41fe4
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-07-21 10:20:42 -0500

    some tests to characterize

---
+++ src/BQ.Persistence.Tests/World__CreateMap__should.vb
@@ -0,0 +1,21 @@
+﻿Imports BQ.Data
+Imports Shouldly
+Imports Xunit
+
+Public Class World__CreateMap__should
+    <Fact>
+    Public Sub create_a_map()
+        Const mapType = "MapType"
+        Const columns = 3
+        Const rows = 4
+        Const terrainType = "TerrainType"
+        Dim data = New Data.WorldData
+        Dim subject As IWorld = New World(data)
+        Dim actual = subject.CreateMap(mapType, (columns, rows), terrainType)
+        actual.MapType.ShouldBe(mapType)
+        actual.Columns.ShouldBe(columns)
+        actual.Rows.ShouldBe(rows)
+        data.Maps.ShouldHaveSingleItem()
+        data.Maps.Single.Cells.ShouldAllBe(Function(x) x.TerrainType = terrainType AndAlso Not x.CharacterIds.Any)
+    End Sub
+End Class
--- src/BQ.Persistence.Tests/World__Save__should.vb
+++ src/BQ.Persistence.Tests/World__Save__should.vb
@@ -3,16 +3,13 @@ Imports System.IO
 Imports BQ.Data
 Imports Shouldly
 Imports Xunit
-
-Namespace BQ.Persistence.Tests
-    Public Class World__Save__should
-        <Fact>
-        Sub save_to_a_file()
-            Dim filename = Guid.NewGuid.ToString
-            Dim subject As IWorld = New World(New WorldData)
-            subject.Save(filename)
-            File.ReadAllText(filename).ShouldBe("{""Maps"":[],""Characters"":[],""AvatarCharacterId"":null,""Messages"":[],""Items"":[]}")
-        End Sub
-    End Class
-End Namespace
+Public Class World__Save__should
+    <Fact>
+    Sub save_to_a_file()
+        Dim filename = Guid.NewGuid.ToString
+        Dim subject As IWorld = New World(New WorldData)
+        subject.Save(filename)
+        File.ReadAllText(filename).ShouldBe("{""Maps"":[],""Characters"":[],""AvatarCharacterId"":null,""Messages"":[],""Items"":[]}")
+    End Sub
+End Class
 

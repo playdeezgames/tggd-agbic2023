1352c44725a749d7c2f39451c6c89288bd44c9e2
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-07-26 14:12:48 -0500

    holder extensions

---
+++ src/BQ.Persistence/Holders/FluentHolderExtensions.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Runtime.CompilerServices
+
+Public Module FluentHolderExtensions
+    <Extension>
+    Public Function SetMetadata(Of THolder As IMetadataHolder)(holder As THolder, identifier As String, value As String) As THolder
+        holder.Metadata(identifier) = value
+        Return holder
+    End Function
+    <Extension>
+    Public Function SetStatistic(Of THolder As IStatisticsHolder)(holder As THolder, statisticType As String, value As Integer) As THolder
+        holder.Statistic(statisticType) = value
+        Return holder
+    End Function
+    <Extension>
+    Public Function SetFlag(Of THolder As IFlagHolder)(holder As THolder, flagType As String, value As Boolean) As THolder
+        holder.Flag(flagType) = value
+        Return holder
+    End Function
+End Module

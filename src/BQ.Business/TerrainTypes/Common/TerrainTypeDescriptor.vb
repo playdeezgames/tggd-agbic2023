667ec971659ba4197f3a91cd44eb9ce367ebab67
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-08-04 04:06:15 -0500

    got effect data into terrain types

--- src/BQ.Business/TerrainTypes/Common/TerrainTypeDescriptor.vb
+++ src/BQ.Business/TerrainTypes/Common/TerrainTypeDescriptor.vb
@@ -2,6 +2,7 @@
 
 Friend Class TerrainTypeDescriptor
     Inherits VisibleEntityDescriptor
+    Private ReadOnly Property Effects As IReadOnlyDictionary(Of String, EffectData)
     Friend ReadOnly Property Tenable As Boolean
     Friend ReadOnly Property CanInteract As Boolean
         Get
@@ -15,6 +16,11 @@ Friend Class TerrainTypeDescriptor
             Return VerbTypes.Keys
         End Get
     End Property
+    Friend ReadOnly Property AllEffectTypes As IEnumerable(Of String)
+        Get
+            Return Effects.Keys
+        End Get
+    End Property
     Friend ReadOnly Property CellInitializer As Action(Of ICell)
     Sub New(
            name As String,
@@ -23,12 +29,14 @@ Friend Class TerrainTypeDescriptor
            Optional tenable As Boolean = True,
            Optional verbTypes As IReadOnlyDictionary(Of String, Action(Of ICharacter, ICell)) = Nothing,
            Optional cellInitializer As Action(Of ICell) = Nothing,
-           Optional foragables As IReadOnlyDictionary(Of String, Integer) = Nothing)
+           Optional foragables As IReadOnlyDictionary(Of String, Integer) = Nothing,
+           Optional effects As IReadOnlyDictionary(Of String, EffectData) = Nothing)
         MyBase.New(name, glyph, hue)
         Me.Tenable = tenable
         Me.VerbTypes = If(verbTypes, New Dictionary(Of String, Action(Of ICharacter, ICell)))
         Me.CellInitializer = If(cellInitializer, AddressOf DoNothing)
         Me.Foragables = If(foragables, New Dictionary(Of String, Integer) From {{"", 1}})
+        Me.Effects = If(effects, New Dictionary(Of String, EffectData))
     End Sub
 
     Private Sub DoNothing(cell As ICell)

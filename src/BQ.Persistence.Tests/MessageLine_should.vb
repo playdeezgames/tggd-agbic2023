ffc58a3afb31ef1e1259796f0d3f5cfd09ad4cd6
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-08-10 16:49:50 -0500

    MessageLine_should

---
+++ src/BQ.Persistence.Tests/MessageLine_should.vb
@@ -0,0 +1,15 @@
+﻿Public Class MessageLine_should
+    Const Text = "text"
+    Const Hue = 1
+
+    <Fact>
+    Sub has_properties()
+        Dim subject As IMessageLine = CreateSubject()
+        subject.Text.ShouldBe(Text)
+        subject.Hue.ShouldBe(Hue)
+    End Sub
+
+    Private Function CreateSubject() As IMessageLine
+        Return (New World(New Data.WorldData)).CreateMessage().AddLine(Hue, Text).Lines.Single
+    End Function
+End Class

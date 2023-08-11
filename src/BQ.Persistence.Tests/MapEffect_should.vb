269770c9cc240cdd483c7010f3eba8513784d572
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-08-11 03:49:42 -0500

    map effects

--- src/BQ.Persistence.Tests/MapEffect_should.vb
+++ src/BQ.Persistence.Tests/MapEffect_should.vb
@@ -1,3 +1,23 @@
 ﻿Public Class MapEffect_should
+    <Fact>
+    Sub hold_statistics()
+        Dim subject As IMapEffect = CreateSubject()
+        DoStatisticHolderTests(subject)
+    End Sub
+
+    Private Function CreateSubject() As IMapEffect
+        Return (New World(New Data.WorldData())).CreateMap("MapType", (1, 1), "TerrainType").CreateEffect()
+    End Function
+
+    <Fact>
+    Sub hold_metadata()
+        Dim subject As IMapEffect = CreateSubject()
+        DoMetadataHolderTests(subject)
+    End Sub
+    <Fact>
+    Sub hold_flags()
+        Dim subject As IMapEffect = CreateSubject()
+        DoFlagHolderTests(subject)
+    End Sub
 
 End Class

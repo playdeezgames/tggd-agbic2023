dfc3b19c1854a44abee356aed67decbe0335196b
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-08-20 05:14:21 -0500

    ActionMenuState

--- src/BQ.Presentation/States/Menus/ActionMenuState.vb
+++ src/BQ.Presentation/States/Menus/ActionMenuState.vb
@@ -1,62 +1,44 @@
 ﻿Friend Class ActionMenuState
     Inherits BasePickerState(Of IWorldModel, String)
 
-    Public Sub New(parent As IGameController, setState As Action(Of String, Boolean), context As IUIContext(Of IWorldModel))
-        MyBase.New(parent, setState, context, "Actions", context.ControlsText("Select", "Cancel"), Neutral)
+    Public Sub New(
+                  parent As IGameController,
+                  setState As Action(Of String, Boolean),
+                  context As IUIContext(Of IWorldModel))
+        MyBase.New(
+            parent,
+            setState,
+            context,
+            ActionsText,
+            context.ControlsText(SelectText, CancelText),
+            Neutral)
     End Sub
 
     Protected Overrides Sub OnActivateMenuItem(value As (String, String))
-        Select Case value.Item2
-            Case Constants.StatisticsText
-                SetState(GameState.Statistics)
-            Case Constants.GroundText
-                SetState(GameState.Ground)
-            Case Constants.InventoryText
-                SetState(GameState.Inventory)
-            Case Constants.ForageText
-                SetState(GameState.Forage)
-            Case Constants.MakeTwineText
-                SetState(GameState.MakeTwine)
-            Case Constants.EquipmentText
-                SetState(GameState.Equipment)
-            Case Constants.SleepText
-                SetState(GameState.Sleep)
-            Case Constants.BuildFireText
-                SetState(GameState.BuildFire)
-            Case Constants.PutOutFireText
-                SetState(GameState.PutOutFire)
-            Case Constants.MakeTorchText
-                SetState(GameState.MakeTorch)
-            Case Constants.MakeHatchetText
-                SetState(GameState.MakeHatchet)
-            Case Constants.KnapText
-                SetState(GameState.Knap)
-            Case Constants.BuildFurnaceText
-                SetState(GameState.BuildFurnace)
-            Case Constants.CookBagelText
-                SetState(GameState.CookBagel)
-        End Select
+        SetState(actionTable(value.Item2))
     End Sub
 
-    Private ReadOnly actionConditions As IReadOnlyList(Of (text As String, condition As Func(Of IWorldModel, Boolean))) =
-        New List(Of (String, Func(Of IWorldModel, Boolean))) From
+    Private ReadOnly actionConditions As IReadOnlyList(
+        Of (text As String, condition As Func(Of IWorldModel, Boolean), state As String)) =
+        New List(Of (String, Func(Of IWorldModel, Boolean), String)) From
         {
-            (GroundText, Function(m) m.Map.HasItems((0, 0))),
-            (InventoryText, Function(m) m.Avatar.HasItems),
-            (EquipmentText, Function(m) m.Avatar.HasEquipment),
-            (SleepText, Function(m) m.Avatar.CanSleep),
-            (ForageText, Function(m) m.Avatar.CanForage),
-            (MakeTwineText, Function(m) m.Avatar.CanMakeTwine),
-            (BuildFireText, Function(m) m.Avatar.CanBuildFire),
-            (PutOutFireText, Function(m) m.Avatar.CanPutOutFire),
-            (MakeTorchText, Function(m) m.Avatar.CanMakeTorch),
-            (BuildFurnaceText, Function(m) m.Avatar.CanBuildFurnace),
-            (CookBagelText, Function(m) m.Avatar.CanCookBagel),
-            (KnapText, Function(m) m.Avatar.CanKnap),
-            (MakeHatchetText, Function(m) m.Avatar.CanMakeHatchet),
-            (StatisticsText, Function(m) True)
+            (GroundText, Function(m) m.Map.HasItems((0, 0)), GameState.Ground),
+            (InventoryText, Function(m) m.Avatar.HasItems, GameState.Inventory),
+            (EquipmentText, Function(m) m.Avatar.HasEquipment, GameState.Equipment),
+            (SleepText, Function(m) m.Avatar.CanSleep, GameState.Sleep),
+            (ForageText, Function(m) m.Avatar.CanForage, GameState.Forage),
+            (MakeTwineText, Function(m) m.Avatar.CanMakeTwine, GameState.MakeTwine),
+            (BuildFireText, Function(m) m.Avatar.CanBuildFire, GameState.BuildFire),
+            (PutOutFireText, Function(m) m.Avatar.CanPutOutFire, GameState.PutOutFire),
+            (MakeTorchText, Function(m) m.Avatar.CanMakeTorch, GameState.MakeTorch),
+            (BuildFurnaceText, Function(m) m.Avatar.CanBuildFurnace, GameState.BuildFurnace),
+            (CookBagelText, Function(m) m.Avatar.CanCookBagel, GameState.CookBagel),
+            (KnapText, Function(m) m.Avatar.CanKnap, GameState.Knap),
+            (MakeHatchetText, Function(m) m.Avatar.CanMakeHatchet, GameState.MakeHatchet),
+            (StatisticsText, Function(m) True, GameState.Statistics)
         }
-
+    Private ReadOnly actionTable As IReadOnlyDictionary(Of String, String) =
+        actionConditions.ToDictionary(Function(x) x.text, Function(x) x.state)
 
     Protected Overrides Function InitializeMenuItems() As List(Of (String, String))
         Return actionConditions.

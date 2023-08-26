9a194c4d575e07e2f643d219fe688c63f20cfd70
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-08-26 05:39:34 -0500

    shipped it!

--- shippit.sh
+++ shippit.sh
@@ -2,7 +2,7 @@ rm -rf ./pub-linux
 rm -rf ./pub-windows
 rm -rf ./pub-mac
 dotnet publish ./src/BagelQuest/BagelQuest.vbproj -o ./pub-linux -c Release --sc -r linux-x64
-dotnet publish ./src/BagelQuest/BagelQuest.vbproj -o ./pub-windows -c Release --sc -r win-x64
+dotnet publish ./src/BagelQuest/BagelQuest.vbproj -o ./pub-windows -c Release --sc -p:PublishSingleFile=true -r win-x64
 dotnet publish ./src/BagelQuest/BagelQuest.vbproj -o ./pub-mac -c Release --sc -r osx-x64
 butler push pub-windows thegrumpygamedev/bagel-quest:windows
 butler push pub-linux thegrumpygamedev/bagel-quest:linux

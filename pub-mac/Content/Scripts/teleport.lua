67d55b4dd2bb959c7d37f3afc623a91c73fa0aa7
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-08-29 05:00:05 -0500

    shipped it!

---
+++ pub-mac/Content/Scripts/teleport.lua
@@ -0,0 +1,10 @@
+﻿function doTeleport(character, effect)
+	local mapId = effect:GetStatistic("MapId")
+	local cellColumn = effect:GetStatistic("CellColumn")
+	local cellRow = effect:GetStatistic("CellRow")
+	local map = world:GetMap(mapId)
+	local nextCell = map:GetCell(cellColumn, cellRow)
+	nextCell:AddCharacter(character)
+	character.Cell:RemoveCharacter(character)
+	character.Cell = nextCell
+end

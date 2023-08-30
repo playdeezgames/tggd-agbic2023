a5ce2dd45c9e017ea726e4593a1ff555458462dc
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-08-30 05:04:06 -0500

    druid allergies

--- src/BQ.Business/CharacterTypes/CharacterTypes.vb
+++ src/BQ.Business/CharacterTypes/CharacterTypes.vb
@@ -38,6 +38,22 @@ character:AddItem(ItemInitializer.CreateItem(character.World,""CookedRat""))",
                     effectScripts:=New Dictionary(Of String, String) From
                     {
                         {
+                            "DruidAllergies",
+                            "        
+character.World:CreateMessage():
+    AddLine(7, ""Alas, I have allergies."")"
+                        },
+                        {
+                            "DruidTalk",
+                            "
+character.World:CreateMessage():
+    AddLine(7, ""Greetings! I am Marcus, the hippy druid.""):
+    AddLine(7, ""I can help you learn nature's way.""):
+    AddChoice(""Cool story, bro!"", ""ExitDialog""):
+    AddChoice(""Don't druids live in the woods?"", ""DruidAllergies""):
+    AddChoice(""Teach me!"", ""DruidTeachMenu""):
+    AddChoice(""What's for sale?"", ""DruidPrices"")"},
+                        {
                             "EatSeasonedRat",
                             "
 local item = CharacterExtensions.ConsumedItem(character, effect)
--- src/BQ.Business/CharacterTypes/EffectHandlers/DruidEffectHandlers.vb
+++ src/BQ.Business/CharacterTypes/EffectHandlers/DruidEffectHandlers.vb
@@ -1,20 +1,4 @@
 ﻿Friend Module DruidEffectHandlers
-
-    Friend Sub DoDruidTalk(character As ICharacter, effect As IEffect)
-        character.World.CreateMessage().
-            AddLine(LightGray, "Greetings! I am Marcus, the hippy druid.").
-            AddLine(LightGray, "I can help you learn nature's way.").
-            AddChoice("Cool story, bro!", "ExitDialog").
-            AddChoice("Don't druids live in the woods?", "DruidAllergies").
-            AddChoice("Teach me!", "DruidTeachMenu").
-            AddChoice("What's for sale?", "DruidPrices")
-    End Sub
-
-    Friend Sub DoDruidAllergies(character As ICharacter, effect As IEffect)
-        character.World.CreateMessage().
-            AddLine(LightGray, "Alas, I have allergies.")
-    End Sub
-
     Friend Sub DoDruidPrices(character As ICharacter, effect As IEffect)
         Dim msg = character.World.CreateMessage().
                         AddLine(LightGray, "I sell a variety of herbs.").

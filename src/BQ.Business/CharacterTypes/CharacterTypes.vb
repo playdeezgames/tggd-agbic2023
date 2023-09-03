1145f7205ddb5c5d57b07853dae9da761fa784be
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-09-03 16:49:50 -0500

    fixd it

--- src/BQ.Business/CharacterTypes/CharacterTypes.vb
+++ src/BQ.Business/CharacterTypes/CharacterTypes.vb
@@ -62,7 +62,7 @@ local Multiplier = 2
 local TrainingCost = Multiplier * CharacterExtensions.MaximumEnergy(character)
 if CharacterExtensions.Jools(character) < TrainingCost then
     msg:
-        AddLine(7, $""The price is {TrainingCost} jools.""):
+        AddLine(7, ""The price is "" .. TrainingCost .. "" jools.""):
         AddLine(7, ""I have overhead, you know."")
     return
 end
@@ -72,7 +72,7 @@ CharacterExtensions.SetMaximumEnergy(character, CharacterExtensions.MaximumEnerg
 CharacterExtensions.AddEnergy(character, 1)
 msg:AddLine(4, CharacterExtensions.Name(character) .. "" loses 1 AP"")
 msg:AddLine(4, CharacterExtensions.Name(character) .. "" loses "" .. TrainingCost .. "" jools"")
-msg:AddLine(2, CharacterExtensions.Name(character)} .. "" adds 1 Maximum Energy"")
+msg:AddLine(2, CharacterExtensions.Name(character) .. "" adds 1 Maximum Energy"")
 msg:AddLine(7, ""Yer now at "" .. CharacterExtensions.MaximumEnergy(character) .. "" Maximum Energy."")
 msg:AddLine(7, ""Persistence is futile!"")"
                         },

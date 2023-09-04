cb1249bd4a999fae644e628b5a8fe32d36c4c7b6
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-09-04 04:30:46 -0500

    health trainer complete

--- src/BQ.Business/CharacterTypes/CharacterTypes.vb
+++ src/BQ.Business/CharacterTypes/CharacterTypes.vb
@@ -33,6 +33,37 @@
                     effectScripts:=New Dictionary(Of String, String) From
                     {
                         {
+                            "TrainHealth",
+                            "
+local msg = character.World:CreateMessage()
+local Multiplier = 5
+local TrainingCost = Multiplier * CharacterExtensions.MaximumHealth(character)
+if CharacterExtensions.AdvancementPoints(character) < TrainingCost then
+    msg:AddLine(7, ""To go from "" .. CharacterExtensions.MaximumHealth(character) .. "" to "" .. CharacterExtensions.MaximumHealth(character) + 1 .. "" Maximum Health,"")
+    msg:AddLine(7, ""you need "" .. TrainingCost .. "" AP, but you have "" .. CharacterExtensions.AdvancementPoints(character) .. ""."")
+    msg:AddLine(7, ""Come back when yer more experienced!"")
+    return
+end
+CharacterExtensions.AddAdvancementPoints(character, -TrainingCost)
+CharacterExtensions.SetMaximumHealth(character, CharacterExtensions.MaximumHealth(character) + 1)
+CharacterExtensions.SetHealth(character, CharacterExtensions.Health(character) + 1)
+msg:AddLine(4, CharacterExtensions.Name(character) .. "" loses "" .. TrainingCost .. "" AP"")
+msg:AddLine(2, CharacterExtensions.Name(character) .. "" adds 1 Maximum Health"")
+msg:AddLine(7, ""Yer now at "" .. CharacterExtensions.MaximumHealth(character) .. "" Maximum Health."")
+msg:AddLine(7, ""Remember! if you don't have yer health,"")
+msg:AddLine(7, ""you don't really have anything!"")"
+                        },
+                        {
+                            "HealthTrainerTalk",
+                            "
+character.World:CreateMessage():
+    AddLine(7, ""I am the health trainer!""):
+    AddLine(7, ""I can help you increase yer health.""):
+    AddLine(7, ""The cost is "" .. CharacterExtensions.MaximumHealth(character) * 5 .. "" AP.""):
+    AddChoice(""Cool story, bro!"", ""ExitDialog""):
+    AddChoice(""Train me!"", ""TrainHealth"")"
+                        },
+                        {
                             "Heal",
                             "
 local maximumHealth = math.min(CharacterExtensions.MaximumHealth(character), effect:GetStatistic(""MaximumHealth""))
--- src/BQ.Business/CharacterTypes/EffectHandlers/LoxyEffectHandlers.vb
+++ src/BQ.Business/CharacterTypes/EffectHandlers/LoxyEffectHandlers.vb
@@ -2,8 +2,6 @@
     Friend All As IReadOnlyDictionary(Of String, Action(Of ICharacter, IEffect)) =
         New Dictionary(Of String, Action(Of ICharacter, IEffect)) From
                     {
-                        {"TrainHealth", AddressOf DoTrainHealth},
-                        {"HealthTrainerTalk", AddressOf DoHealthTrainerTalk},
                         {"GorachanTalk", AddressOf DoGorachanTalk},
                         {"PervertInnkeeper", AddressOf DoPerventInnkeeper},
                         {"PayInnkeeper", AddressOf DoPayInnkeeper},

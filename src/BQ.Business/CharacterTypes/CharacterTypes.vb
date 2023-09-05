f5baffafd158fa5927f1ff8ef8ca4e683c2f7a24
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-09-05 04:10:21 -0500

    buy

--- src/BQ.Business/CharacterTypes/CharacterTypes.vb
+++ src/BQ.Business/CharacterTypes/CharacterTypes.vb
@@ -33,6 +33,25 @@ character:AddItem(ItemInitializer.CreateItem(character.World,""ClayPot""))",
                     effectScripts:=New Dictionary(Of String, String) From
                     {
                         {
+                            "Buy",
+                            "
+        local itemType = effect:GetMetadata(""ItemType"")
+        local price = effect:GetStatistic(""Price"")
+        if CharacterExtensions.Jools(character) < price then
+            character.World:
+                CreateMessage():
+                AddLine(7, ""You don't have enough!""):
+                AddChoice(""Shucks!"", effect:GetMetadata(""EffectType""))
+            return
+        end
+        CharacterExtensions.AddJools(character, -price)
+        character:AddItem(ItemInitializer.CreateItem(character.World, itemType))
+        character.World:
+            CreateMessage():
+            AddLine(7, ""Thank you for yer purchase!""):
+            AddChoice(""No worries!"", effect:GetMetadata(""EffectType""))"
+                        },
+                        {
                             "BumpRiver",
                             "
         local msg = character.World:CreateMessage():
--- src/BQ.Business/CharacterTypes/EffectHandlers/LoxyEffectHandlers.vb
+++ src/BQ.Business/CharacterTypes/EffectHandlers/LoxyEffectHandlers.vb
@@ -2,7 +2,6 @@
     Friend All As IReadOnlyDictionary(Of String, Action(Of ICharacter, IEffect)) =
         New Dictionary(Of String, Action(Of ICharacter, IEffect)) From
                     {
-                        {"Buy", AddressOf DoBuy},
                         {"Forage", AddressOf DoForage}
                     }
     Private Sub DoForage(character As ICharacter, effect As IEffect)
@@ -18,23 +17,5 @@
         Dim item = ItemInitializer.CreateItem(character.World, itemType)
         character.AddItem(item)
         character.World.CreateMessage().AddLine(7, $"{CharacterExtensions.Name(character)} finds {ItemExtensions.Name(item)}")
-    End Sub
-
-    Private Sub DoBuy(character As ICharacter, effect As IEffect)
-        Dim itemType = effect.GetMetadata("ItemType")
-        Dim price = effect.GetStatistic("Price")
-        If CharacterExtensions.Jools(character) < price Then
-            character.World.
-                CreateMessage().
-                AddLine(7, "You don't have enough!").
-                AddChoice("Shucks!", effect.GetMetadata("EffectType"))
-            Return
-        End If
-        CharacterExtensions.AddJools(character, -price)
-        character.AddItem(ItemInitializer.CreateItem(character.World, itemType))
-        character.World.
-            CreateMessage().
-            AddLine(7, "Thank you for yer purchase!").
-            AddChoice("No worries!", effect.GetMetadata("EffectType"))
     End Sub
 End Module

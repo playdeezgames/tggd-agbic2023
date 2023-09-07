372135bba4fe3210293a96f3dfdc82f97b6eb9a5
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-09-07 04:57:01 -0500

    making a lot of headway on world initializer

--- src/BQ.Business/Initializers/WorldInitializer.vb
+++ src/BQ.Business/Initializers/WorldInitializer.vb
@@ -17,68 +17,120 @@ Friend Module WorldInitializer
     Private Sub StitchPotterToTown(world As IWorld)
         Dim townMap = WorldExtensions.GetSingleMapByMapType(world, MapTypes.Town)
         Dim potterMap = WorldExtensions.GetSingleMapByMapType(world, MapTypes.Potter)
+        FromTownToPotter(townMap, potterMap)
+        FromPotterToTown(townMap, potterMap)
+    End Sub
+
+    Private Sub FromPotterToTown(townMap As IMap, potterMap As IMap)
+        Dim cell As ICell = potterMap.GetCell(PotterColumns \ 2, PotterRows - 1)
+        cell.Effect = potterMap.CreateEffect()
+        TriggerExtensions.SetEffectType(cell.Effect, "Teleport")
+        TriggerExtensions.SetDestination(cell.Effect, townMap.GetCell(13, 12))
+    End Sub
+
+    Private Sub FromTownToPotter(townMap As IMap, potterMap As IMap)
         townMap.GetCell(13, 13).Effect =
             townMap.CreateEffect()
         TriggerExtensions.SetEffectType(townMap.GetCell(13, 13).Effect, "Teleport")
         TriggerExtensions.SetDestination(townMap.GetCell(13, 13).Effect, potterMap.GetCell(PotterColumns \ 2, PotterRows - 2))
-        potterMap.GetCell(PotterColumns \ 2, PotterRows - 1).Effect =
-            potterMap.CreateEffect()
-        TriggerExtensions.SetEffectType(potterMap.GetCell(PotterColumns \ 2, PotterRows - 1).Effect, "Teleport")
-        TriggerExtensions.SetDestination(potterMap.GetCell(PotterColumns \ 2, PotterRows - 1).Effect, townMap.GetCell(13, 12))
     End Sub
 
     Private Sub StitchEnergyTrainerToTown(world As IWorld)
         Dim townMap = WorldExtensions.GetSingleMapByMapType(world, MapTypes.Town)
         Dim trainerMap = WorldExtensions.GetSingleMapByMapType(world, MapTypes.EnergyTrainer)
-        townMap.GetCell(6, 6).Effect = townMap.CreateEffect()
-        TriggerExtensions.SetEffectType(townMap.GetCell(6, 6).Effect, "Teleport")
-        TriggerExtensions.SetDestination(townMap.GetCell(6, 6).Effect, trainerMap.GetCell(EnergyTrainerColumns \ 2, 1))
-        trainerMap.GetCell(EnergyTrainerColumns \ 2, 0).Effect = trainerMap.CreateEffect()
-        TriggerExtensions.SetEffectType(trainerMap.GetCell(EnergyTrainerColumns \ 2, 0).Effect, "Teleport")
-        TriggerExtensions.SetDestination(trainerMap.GetCell(EnergyTrainerColumns \ 2, 0).Effect, townMap.GetCell(6, 5))
+        FromTownToEnergyTrainer(townMap, trainerMap)
+        FromEnergyTrainerToTown(townMap, trainerMap)
+    End Sub
+
+    Private Sub FromEnergyTrainerToTown(townMap As IMap, trainerMap As IMap)
+        Dim cell As ICell = trainerMap.GetCell(EnergyTrainerColumns \ 2, 0)
+        cell.Effect = trainerMap.CreateEffect()
+        TriggerExtensions.SetEffectType(cell.Effect, "Teleport")
+        TriggerExtensions.SetDestination(cell.Effect, townMap.GetCell(6, 5))
+    End Sub
+
+    Private Sub FromTownToEnergyTrainer(townMap As IMap, trainerMap As IMap)
+        Dim cell As ICell = townMap.GetCell(6, 6)
+        cell.Effect = townMap.CreateEffect()
+        TriggerExtensions.SetEffectType(cell.Effect, "Teleport")
+        TriggerExtensions.SetDestination(cell.Effect, trainerMap.GetCell(EnergyTrainerColumns \ 2, 1))
     End Sub
 
     Private Sub StitchInnToTown(world As IWorld)
         Dim townMap = WorldExtensions.GetSingleMapByMapType(world, MapTypes.Town)
         Dim innMap = WorldExtensions.GetSingleMapByMapType(world, MapTypes.Inn)
         Dim cellarMap = WorldExtensions.GetSingleMapByMapType(world, MapTypes.Cellar)
-        townMap.GetCell(3, 3).Effect = townMap.CreateEffect()
-        TriggerExtensions.SetEffectType(townMap.GetCell(3, 3).Effect, "Teleport")
-        TriggerExtensions.SetDestination(townMap.GetCell(3, 3).Effect, innMap.GetCell(InnColumns \ 2, InnRows - 2))
-        innMap.GetCell(InnColumns \ 2, InnRows - 1).Effect = innMap.CreateEffect()
-        TriggerExtensions.SetEffectType(innMap.GetCell(InnColumns \ 2, InnRows - 1).Effect, "Teleport")
-        TriggerExtensions.SetDestination(innMap.GetCell(InnColumns \ 2, InnRows - 1).Effect, townMap.GetCell(3, 2))
-        Dim downStairs = innMap.Cells.Single(Function(x) x.TerrainType = "StairsDown")
-        Dim upStairs = cellarMap.Cells.Single(Function(x) x.TerrainType = "StairsUp")
+        FromTownToInn(townMap, innMap)
+        FromInnToTown(townMap, innMap)
+        TransitionsBetweenInnAndCellar(innMap, cellarMap)
+    End Sub
+
+    Private Sub TransitionsBetweenInnAndCellar(innMap As IMap, cellarMap As IMap)
+        Dim downStairs = innMap.Cells.Single(Function(x) x.TerrainType = "StairsDown") 'TODO: 
+        Dim upStairs = cellarMap.Cells.Single(Function(x) x.TerrainType = "StairsUp") 'TODO: 
 
         TriggerExtensions.SetDestination(downStairs.Effect, upStairs)
         TriggerExtensions.SetDestination(upStairs.Effect, downStairs)
     End Sub
 
+    Private Sub FromInnToTown(townMap As IMap, innMap As IMap)
+        Dim cell As ICell = innMap.GetCell(InnColumns \ 2, InnRows - 1)
+        cell.Effect = innMap.CreateEffect()
+        TriggerExtensions.SetEffectType(cell.Effect, "Teleport")
+        TriggerExtensions.SetDestination(cell.Effect, townMap.GetCell(3, 2))
+    End Sub
+
+    Private Sub FromTownToInn(townMap As IMap, innMap As IMap)
+        Dim cell As ICell = townMap.GetCell(3, 3)
+        cell.Effect = townMap.CreateEffect()
+        TriggerExtensions.SetEffectType(cell.Effect, "Teleport")
+        TriggerExtensions.SetDestination(cell.Effect, innMap.GetCell(InnColumns \ 2, InnRows - 2))
+    End Sub
+
     Private Sub StitchDruidHouseToTown(world As IWorld)
         Dim townMap = WorldExtensions.GetSingleMapByMapType(world, MapTypes.Town)
         Dim druidMap = WorldExtensions.GetSingleMapByMapType(world, MapTypes.DruidHouse)
-        townMap.GetCell(10, 10).Effect =
+        FromTownToDruid(townMap, druidMap)
+        FromDruidToTown(townMap, druidMap)
+    End Sub
+
+    Private Sub FromDruidToTown(townMap As IMap, druidMap As IMap)
+        Dim cell As ICell = druidMap.GetCell(0, DruidHouseRows \ 2)
+        cell.Effect =
+                    druidMap.CreateEffect()
+        TriggerExtensions.SetEffectType(cell.Effect, "Teleport")
+        TriggerExtensions.SetDestination(cell.Effect, townMap.GetCell(9, 10))
+    End Sub
+
+    Private Sub FromTownToDruid(townMap As IMap, druidMap As IMap)
+        Dim cell As ICell = townMap.GetCell(10, 10)
+        cell.Effect =
             townMap.CreateEffect()
-        TriggerExtensions.SetEffectType(townMap.GetCell(10, 10).Effect, "Teleport")
-        TriggerExtensions.SetDestination(townMap.GetCell(10, 10).Effect, druidMap.GetCell(1, DruidHouseRows \ 2))
-        druidMap.GetCell(0, DruidHouseRows \ 2).Effect =
-            druidMap.CreateEffect()
-        TriggerExtensions.SetEffectType(druidMap.GetCell(0, DruidHouseRows \ 2).Effect, "Teleport")
-        TriggerExtensions.SetDestination(druidMap.GetCell(0, DruidHouseRows \ 2).Effect, townMap.GetCell(9, 10))
+        TriggerExtensions.SetEffectType(cell.Effect, "Teleport")
+        TriggerExtensions.SetDestination(cell.Effect, druidMap.GetCell(1, DruidHouseRows \ 2))
     End Sub
 
     Private Sub StitchHealthTrainerToTown(world As IWorld)
         Dim townMap = WorldExtensions.GetSingleMapByMapType(world, MapTypes.Town)
         Dim trainerMap = WorldExtensions.GetSingleMapByMapType(world, MapTypes.HealthTrainer)
-        townMap.GetCell(8, 4).Effect =
+        FromTownToHealthTrainer(townMap, trainerMap)
+        FromHealthTrainerToTown(townMap, trainerMap)
+    End Sub
+
+    Private Sub FromHealthTrainerToTown(townMap As IMap, trainerMap As IMap)
+        Dim cell As ICell = trainerMap.GetCell(HealthTrainerColumns \ 2, HealthTrainerRows - 1)
+        cell.Effect =
+                    trainerMap.CreateEffect()
+        TriggerExtensions.SetEffectType(cell.Effect, "Teleport")
+        TriggerExtensions.SetDestination(cell.Effect, townMap.GetCell(8, 3))
+    End Sub
+
+    Private Sub FromTownToHealthTrainer(townMap As IMap, trainerMap As IMap)
+        Dim cell As ICell = townMap.GetCell(8, 4)
+        cell.Effect =
             townMap.CreateEffect()
-        TriggerExtensions.SetEffectType(townMap.GetCell(8, 4).Effect, "Teleport")
-        TriggerExtensions.SetDestination(townMap.GetCell(8, 4).Effect, trainerMap.GetCell(HealthTrainerColumns \ 2, HealthTrainerRows - 2))
-        trainerMap.GetCell(HealthTrainerColumns \ 2, HealthTrainerRows - 1).Effect =
-            trainerMap.CreateEffect()
-        TriggerExtensions.SetEffectType(trainerMap.GetCell(HealthTrainerColumns \ 2, HealthTrainerRows - 1).Effect, "Teleport")
-        TriggerExtensions.SetDestination(trainerMap.GetCell(HealthTrainerColumns \ 2, HealthTrainerRows - 1).Effect, townMap.GetCell(8, 3))
+        TriggerExtensions.SetEffectType(cell.Effect, "Teleport")
+        TriggerExtensions.SetDestination(cell.Effect, trainerMap.GetCell(HealthTrainerColumns \ 2, HealthTrainerRows - 2))
     End Sub
 
     Private Sub StitchHealerToTown(world As IWorld)
@@ -98,35 +150,59 @@ Friend Module WorldInitializer
     End Sub
 
     Private Sub FromTownToHealerHouse(townMap As IMap, healerMap As IMap)
-        townMap.GetCell(3, 13).Effect =
+        Dim fromCell As ICell = townMap.GetCell(3, 13)
+        fromCell.Effect =
             townMap.CreateEffect()
-        TriggerExtensions.SetEffectType(townMap.GetCell(3, 13).Effect, "Teleport")
-        TriggerExtensions.SetDestination(townMap.GetCell(3, 13).Effect, healerMap.GetCell(HealerColumns \ 2, HealerRows - 2))
+        TriggerExtensions.SetEffectType(fromCell.Effect, "Teleport")
+        Dim toCell As ICell = healerMap.GetCell(HealerColumns \ 2, HealerRows - 2)
+        TriggerExtensions.SetDestination(fromCell.Effect, toCell)
     End Sub
 
     Private Sub StitchTownToWilderness(world As IWorld)
         Dim townMap = WorldExtensions.GetSingleMapByMapType(world, MapTypes.Town)
         Dim wildernessMap = WorldExtensions.GetSingleMapByMapType(world, MapTypes.Wilderness)
-        Dim townCell = wildernessMap.Cells.Single(Function(x) x.TerrainType = "Town")
+        Dim townCell As ICell = FromWildernessToTown(townMap, wildernessMap)
+        FromTownToWildernessSouth(townMap, townCell)
+        FromTownToWildernessNorth(townMap, townCell)
+        FromTownToWildernessWest(townMap, townCell)
+        FromTownToWildernessEast(townMap, townCell)
+    End Sub
+
+    Private Function FromWildernessToTown(townMap As IMap, wildernessMap As IMap) As ICell
+        Dim fromCell = wildernessMap.Cells.Single(Function(x) x.TerrainType = "Town")
+        fromCell.Effect =
+                    wildernessMap.CreateEffect()
+        TriggerExtensions.SetEffectType(fromCell.Effect, "Teleport")
+        Dim toCell As ICell = townMap.GetCell(townMap.Columns \ 2, townMap.Rows - 2)
+        TriggerExtensions.SetDestination(fromCell.Effect, toCell)
+        Return fromCell
+    End Function
+
+    Private Sub FromTownToWildernessEast(townMap As IMap, townCell As ICell)
+        townMap.GetCell(townMap.Columns - 1, townMap.Rows \ 2).Effect =
+                    townMap.CreateEffect()
+        TriggerExtensions.SetEffectType(townMap.GetCell(townMap.Columns - 1, townMap.Rows \ 2).Effect, "Teleport")
+        TriggerExtensions.SetDestination(townMap.GetCell(townMap.Columns - 1, townMap.Rows \ 2).Effect, townCell)
+    End Sub
+
+    Private Sub FromTownToWildernessWest(townMap As IMap, townCell As ICell)
+        townMap.GetCell(0, townMap.Rows \ 2).Effect =
+                    townMap.CreateEffect()
+        TriggerExtensions.SetEffectType(townMap.GetCell(0, townMap.Rows \ 2).Effect, "Teleport")
+        TriggerExtensions.SetDestination(townMap.GetCell(0, townMap.Rows \ 2).Effect, townCell)
+    End Sub
+
+    Private Sub FromTownToWildernessNorth(townMap As IMap, townCell As ICell)
+        townMap.GetCell(townMap.Columns \ 2, 0).Effect =
+                    townMap.CreateEffect()
+        TriggerExtensions.SetEffectType(townMap.GetCell(townMap.Columns \ 2, 0).Effect, "Teleport")
+        TriggerExtensions.SetDestination(townMap.GetCell(townMap.Columns \ 2, 0).Effect, townCell)
+    End Sub
+
+    Private Sub FromTownToWildernessSouth(townMap As IMap, townCell As ICell)
         townMap.GetCell(townMap.Columns \ 2, townMap.Rows - 1).Effect =
             townMap.CreateEffect()
         TriggerExtensions.SetEffectType(townMap.GetCell(townMap.Columns \ 2, townMap.Rows - 1).Effect, "Teleport")
         TriggerExtensions.SetDestination(townMap.GetCell(townMap.Columns \ 2, townMap.Rows - 1).Effect, townCell)
-        townMap.GetCell(townMap.Columns \ 2, 0).Effect =
-            townMap.CreateEffect()
-        TriggerExtensions.SetEffectType(townMap.GetCell(townMap.Columns \ 2, 0).Effect, "Teleport")
-        TriggerExtensions.SetDestination(townMap.GetCell(townMap.Columns \ 2, 0).Effect, townCell)
-        townMap.GetCell(0, townMap.Rows \ 2).Effect =
-            townMap.CreateEffect()
-        TriggerExtensions.SetEffectType(townMap.GetCell(0, townMap.Rows \ 2).Effect, "Teleport")
-        TriggerExtensions.SetDestination(townMap.GetCell(0, townMap.Rows \ 2).Effect, townCell)
-        townMap.GetCell(townMap.Columns - 1, townMap.Rows \ 2).Effect =
-            townMap.CreateEffect()
-        TriggerExtensions.SetEffectType(townMap.GetCell(townMap.Columns - 1, townMap.Rows \ 2).Effect, "Teleport")
-        TriggerExtensions.SetDestination(townMap.GetCell(townMap.Columns - 1, townMap.Rows \ 2).Effect, townCell)
-        townCell.Effect =
-            wildernessMap.CreateEffect()
-        TriggerExtensions.SetEffectType(townCell.Effect, "Teleport")
-        TriggerExtensions.SetDestination(townCell.Effect, townMap.GetCell(townMap.Columns \ 2, townMap.Rows - 2))
     End Sub
 End Module
